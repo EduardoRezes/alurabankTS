c83bb8cab4d4a29baf86ffdf2034a26dbe08371a
Eduardo <contatoeduardorezes@gmail.com>
2022-02-14 15:39:17 -0300

    Programação defensiva

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -5,11 +5,12 @@ export class Negociacao {
         //todos podem ter acesso a essa propriedade, porem ninguem pode manipula-la, somente leitura.
         public readonly _valor: number) {}
 
-    get data(): Date { return this._data};
+    get data(): Date { 
+        const data = new Date(this._data.getTime());
+        return data
+    };
 
     get quantidade(): number { return this._quantidade};
-
-    get valor():number { return this._valor};
 
     get volume():number { return this._quantidade * this._valor};
 }
