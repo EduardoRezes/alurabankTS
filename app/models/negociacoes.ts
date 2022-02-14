87caf698f48ed2fbad4ae4f3f2757e3cd7a017c0
Eduardo <contatoeduardorezes@gmail.com>
2022-02-14 15:31:11 -0300

    suca sintax

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,13 +1,13 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
-    private negociacoes: Array<Negociacao> = [];
+    private negociacoes: Negociacao[] = [];
 
     add(negociacao: Negociacao){
         this.negociacoes.push(negociacao);
     }
 
-    list(): ReadonlyArray<Negociacao>{
+    list(): readonly Negociacao[]{
         //... utiliza uma copia
         return this.negociacoes;
     }
