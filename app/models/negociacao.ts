760eba363c32a8db5d3d9f91fbc26e90a7fdd327
Eduardo <contatoeduardorezes@gmail.com>
2022-02-15 17:41:33 -0300

    Método statico

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -13,4 +13,15 @@ export class Negociacao {
     get quantidade(): number { return this._quantidade};
 
     get volume():number { return this._quantidade * this._valor};
+
+    //todo método static pode ser chamado direto da classe, sem precisar instaciar a classe.
+    public static criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao{
+          //Regular expression
+          const exp = /-/g;
+          //get value on form and format type
+          const date = new Date(dataString.replace(exp, ','));
+          const quantidade = parseInt(quantidadeString);
+          const valor = parseFloat(valorString);
+          return new Negociacao(date, quantidade, valor);
+    }
 }
