604646f21582ab09bd651a695aca997955fddc5c
Eduardo <contatoeduardorezes@gmail.com>
2022-02-15 17:02:49 -0300

    Ajustando visibilidade metodo template

--- app/views/view.ts
+++ app/views/view.ts
@@ -5,11 +5,12 @@ export abstract class View<T> {
     constructor(seletor: string){
         this.elemento = document.querySelector(seletor);
     }
-
-    abstract template(model: T): string;
-    
+        
     update(model: T): void{
         const template = this.template(model);
         this.elemento.innerHTML = template;
     }
+
+    //protected apenas pai e filhas podem ter acesso.
+    protected abstract template(model: T): string;
 }
