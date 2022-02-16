5e32ea4e46f1004cf057471e448fb3462f620369
Eduardo <contatoeduardorezes@gmail.com>
2022-02-16 11:40:32 -0300

    Add tipos de retorno do DOM

--- app/views/view.ts
+++ app/views/view.ts
@@ -5,7 +5,13 @@ export abstract class View<T> {
 
     //com o ? em frente ao escapar, significa que esté parametro  é opcional a ser passado por parametro.
     constructor(seletor: string, escapar?: boolean){
-        this.elemento = document.querySelector(seletor);
+        const elemento = document.querySelector(seletor);
+        if (elemento) {
+            this.elemento = elemento as HTMLElement;
+        }else {
+            throw new Error(`Seletor ${seletor}não existe no DOM.`);
+            
+        }
         if (escapar) {
             this.escapar = escapar;
         }
