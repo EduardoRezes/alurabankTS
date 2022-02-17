790c13b87e3f0e82105f4f50699bf7d7a0e0d1bf
Eduardo <contatoeduardorezes@gmail.com>
2022-02-17 17:26:16 -0300

    Decorator com parametro

--- app/decorators/logar-tempo-de-execucao.ts
+++ app/decorators/logar-tempo-de-execucao.ts
@@ -1,11 +1,17 @@
-export function logarTempoDeExecucao() {
+export function logarTempoDeExecucao(emSegundos: boolean = false) {
     return function(target: any, propertKey: string, descriptor: PropertyDescriptor) {
         const metodoOriginal = descriptor.value;
         descriptor.value = function(...args: Array<any>) {
+            let divisor = 1;
+            let unidade = 'milisegundos';
+            if (emSegundos) {
+                divisor = 1000;
+                unidade = 'segundos';
+            }
             const t1 = performance.now();
             const retorno = metodoOriginal.apply(this, args);
             const t2 = performance.now();
-            console.log(`${propertKey}, tempo de execução: ${(t2-t1)/100} segundos.`);
+            console.log(`${propertKey}, tempo de execução: ${(t2-t1)/100} ${unidade}.`);
             retorno
         }
         return descriptor;
